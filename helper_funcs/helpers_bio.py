824f66d7632f0ac0738bff947309a4c133061555
Stefan Dieterle <golgoths@yahoo.fr>
2015-12-12 19:17:35 +0100

    Added Render button to query-display.html

--- helper_funcs/helpers_bio.py
+++ helper_funcs/helpers_bio.py
@@ -13,12 +13,3 @@ def parse_fasta_alignment(fasta):
     fasta_parsed = AlignIO.read(fasta, 'fasta')
     fasta_parsed = MultipleSeqAlignment([f.upper() for f in fasta_parsed], annotations=fasta_parsed.annotations)
     return fasta_parsed
-
-
-if __name__ == '__main__':
-    # from helper_funcs.helpers_test import file_to_string
-    # fasta = file_to_string('spa_align_clustal_omega.fasta')
-    al = parse_fasta_alignment('../test_data/spa_align_clustal_omega.fasta')
-    # for a in al:
-    #     print(a.upper())
-    print(al)
