46f2a70d81bfd085f0cbd15c56ba7a45c6ae4590
Stefan Dieterle <golgoths@yahoo.fr>
2015-12-13 19:00:11 +0100

    Added basic model and alignment save function

--- base/models.py
+++ base/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
 # Create your models here.
+
+
+def save_alignment_to_db(name, data):
+    alignment = Alignment.objects.create(name=name)
+    for s in data:
+        seqrec = Seqrecord.objects.create(
+                seq=str(s.seq),
+                alphabet=str(s.seq.alphabet),
+                seq_id=s.id,
+                name=s.name,
+                description=s.description
+        )
+        alignment.seqs.add(seqrec)
+    return alignment.pk
+
+
+class Seqrecord(models.Model):
+    """
+    Sequrecords model
+    """
+    seq = models.TextField()
+    alphabet = models.CharField(max_length=50)
+    seq_id = models.CharField(max_length=50)
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=200)
+
+
+class Alignment(models.Model):
+    """
+    Alignment model
+    """
+    name = models.CharField(max_length=50)
+    seqs = models.ManyToManyField(Seqrecord)
--- base/tests/test_models.py
+++ base/tests/test_models.py
@@ -1,9 +1,10 @@
 from django.test import TestCase
 import io
 from Bio import SeqIO
+from Bio.Alphabet.IUPAC import ExtendedIUPACProtein
 from helper_funcs.helpers_bio import parse_fasta_alignment
 from helper_funcs.helpers_test import file_to_string
-from base.models import Alignment, Seqrecord
+from base.models import Alignment, Seqrecord, save_alignment_to_db
 
 __author__ = 'Stefan Dieterle'
 
@@ -17,9 +18,14 @@ class SeqrecordModelTestCase(TestCase):
         seq_input = io.StringIO(file_to_string('spa_align_clustal_omega.fasta'))
         data = SeqIO.parse(seq_input, 'fasta')
         self.seqs = [d.upper() for d in data]
-        self.seq = Seqrecord.create(
-                seq=self.seqs[0].seq,
-                alphabet=str(self.seqs[0].alphabet)
+        for s in self.seqs:
+            s.seq.alphabet = ExtendedIUPACProtein()
+        self.seq = Seqrecord.objects.create(
+                seq=str(self.seqs[0].seq),
+                alphabet=str(self.seqs[0].seq.alphabet),
+                seq_id=str(self.seqs[0].id),
+                name=str(self.seqs[0].name),
+                description=self.seqs[0].description,
         )
 
     def test_seqrecord_basic(self):
@@ -27,8 +33,8 @@ class SeqrecordModelTestCase(TestCase):
         Tests the basic functionality of Seqrecord
         :return:
         """
-        self.assertEqual(self.seq.alphabet, 'ExtendedIUPACProtein()')
-        self.assertEqual(self.seq.seq, self.seqs[0].seq)
+        self.assertEqual(self.seq.alphabet, 'ExtendedIUPACProtein()', self.seq.alphabet)
+        self.assertEqual(self.seq.seq, str(self.seqs[0].seq), self.seq.seq)
 
 
 class AlignmentModelTestCase(TestCase):
@@ -37,17 +43,36 @@ class AlignmentModelTestCase(TestCase):
     """
 
     def setUp(self):
-        name = 'A. tha. SPA family alignment'
+        self.name = 'A. tha. SPA family alignment'
         align_input = io.StringIO(file_to_string('spa_align_clustal_omega.fasta'))
-        data = parse_fasta_alignment(align_input)
-        self.alignment = Alignment.save_alignment_to_db(name, data)
+        self.data = parse_fasta_alignment(align_input)
+        # self.alignment = Alignment().save_alignment_to_db(name, data)
 
     def test_alignment_basic(self):
         """
         Tests the basic functionality of Alignment
         :return:
         """
-        self.assertEqual(self.alignment.name, 'A. tha. SPA family alignment')
+        alignment = Alignment.objects.create(name=self.name)
+        for s in self.data:
+            seqrec = Seqrecord.objects.create(
+                    seq=str(s.seq),
+                    alphabet=str(s.seq.alphabet),
+                    seq_id=s.id,
+                    name=s.name,
+                    description=s.description
+            )
+            alignment.seqs.add(seqrec)
+        self.assertEqual(alignment.name, 'A. tha. SPA family alignment')
         self.assertEqual(
-                [seq.id for seq in self.alignment.seqs],
+                [seq.seq_id for seq in alignment.seqs.all()],
                 ['AT1G53090.1', 'AT3G15354.1', 'AT2G46340.1', 'AT4G11110.1'])
+
+    def test_save_alignment_to_db(self):
+        save = save_alignment_to_db(self.name, self.data)
+        alignment = Alignment.objects.all()
+        self.assertEqual(alignment[0].name, 'A. tha. SPA family alignment', alignment[0].name)
+        self.assertEqual(
+                [seq.seq_id for seq in alignment[0].seqs.all()],
+                ['AT1G53090.1', 'AT3G15354.1', 'AT2G46340.1', 'AT4G11110.1'])
+        self.assertEqual(save, 2, save)
