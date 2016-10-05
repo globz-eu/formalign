bbd8da1a2114655bd646fa8ab211e151b1fae235
Stefan Dieterle <golgoths@yahoo.fr>
2016-10-05 23:19:01 +0200

    Adapted nonfunctional tests to demo alignment.

--- nonfunctional_tests/test_live_server_speeds.py
+++ nonfunctional_tests/test_live_server_speeds.py
@@ -56,7 +56,7 @@ class BasicUserSpeedLiveServerTestCase(StaticLiveServerTestCase):
                 start = time.time()
                 r = self.client.post(self.live_server_url,
                                      data={'csrfmiddlewaretoken': csrftoken, 'seq_type': 'Protein',
-                                           'align_input': alignment_string})
+                                           'align_input': alignment_string, 'custom_data': 'custom'})
                 roundtrip1 = time.time() - start
                 q_display.append(roundtrip1)
 
