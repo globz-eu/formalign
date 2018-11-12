5a90443c3c1fd5b9f0de83a524128617cf5795ac
Stefan Dieterle <stefand@artcom.de>
2018-11-12 22:44:03 +0100

    Remove unused file

--- settings_admin.py
+++
@@ -1,5 +0,0 @@
-import os
-from formalign.settings import *  # noqa
-
-DATABASES['default']['USER'] = os.environ['DB_USER']  # noqa
-DATABASES['default']['PASSWORD'] = os.environ['DB_PASSWORD']  # noqa
